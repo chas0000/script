40fdec9eec8a313116e8376db99a8adabe4b58d6
chas0000 <86915826+chas0000@users.noreply.github.com>
2026-01-28 19:18:50 +0800

    Update 115_redirect.js

--- 115_redirect.js
+++ 115_redirect.js
@@ -1,13 +1,6 @@
-// 获取原始请求 URL
 let url = $request.url;
+let newUrl = "oof.disk://openurl/" + encodeURIComponent(url);
 
-// 对 URL 进行 encodeURIComponent
-let encoded = encodeURIComponent(url);
-
-// 拼接成 oof.disk scheme
-let newUrl = "oof.disk://openurl/" + encoded;
-
-// 返回 302 跳转
 $done({
   status: 302,
   headers: {

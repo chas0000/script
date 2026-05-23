e67e7cd7ddbfd18f4b8278f45dd2e9fa3db0491a
chas0000 <86915826+chas0000@users.noreply.github.com>
2026-05-23 19:51:15 +0800

    Update and rename ed2k一键复制.js to 115_one_click.js

--- 115_one_click.js
+++ 115_one_click.js
@@ -8,8 +8,8 @@
 // @grant        GM_setClipboard
 // @grant        GM_notification
 // @run-at       document-end
-// @updateURL    https://raw.githubusercontent.com/chas0000/script/main/ed2k一键复制.js
-// @downloadURL  https://raw.githubusercontent.com/chas0000/script/main/ed2k一键复制.js
+// @updateURL    https://raw.githubusercontent.com/chas0000/script/main/115_one_click.js
+// @downloadURL  https://raw.githubusercontent.com/chas0000/script/main/115_one_click.js
 // ==/UserScript==
 
 (function() {
